16a6ceb398c15e29b6796b5be62278745c69ed99
DickyNBS <156597121+Dicky035@users.noreply.github.com>
2026-03-25 11:24:42 +0700

    Update script loading mechanism with error handling
    Replace old loader with a new script loader that uses pcall for error handling.

--- main.lua
+++ main.lua
@@ -1,1 +1,11 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Dicky035/Info/main/src/core.lua"))()
+-- NBS LOADER (AUTO LOAD)
+
+local url = "https://raw.githubusercontent.com/Dicky035/Info/main/script.lua"
+
+local success, err = pcall(function()
+    loadstring(game:HttpGet(url))()
+end)
+
+if not success then
+    warn("❌ Gagal load script:", err)
+end
